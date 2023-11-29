3675bc330e7514398caf5695f932c7cbf719898e
Jason Castillo <122952780+nosajc23@users.noreply.github.com>
2023-11-28 23:54:22 -0600

    updated the create athlete page

--- create_athlete.php
+++ create_athlete.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+require("fileUpload.php");
 // Database connection
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=serverside', 'serveruser', 'gorgonzola7!');
@@ -15,20 +16,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $team = $_POST['team'];
     $sport= $_POST['sport'];
     $bio = $_POST['bio'];
-
-      // Image upload handling
-      //$imagePath = handleImageUpload();
-
-     // Validate and process image upload
-    //  $imageType = exif_imagetype($tmpName);
+    $image_path= $_POST['image_path'];
  
         // Insert athlete data into the "athletes" table
         $stmt = $pdo->prepare("INSERT INTO athletes (athlete_name, team, sport, bio) VALUES (?, ?, ?, ?)");
-
-        // Insert image path into the "images" table
-        // $stmt = $pdo->prepare("INSERT INTO images (filename) VALUES (?)");
-        // $stmt->execute([$uploadPath]);
-
     
         header('Location: AdminPage.php'); // Redirect to the home page
 
@@ -39,34 +30,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         } 
     
 }
-
-// Function to handle image upload
-// function handleImageUpload() {
-//     if ($_FILES['image']['error'] === UPLOAD_ERR_OK) {
-//         $tmpName = $_FILES['image']['tmp_name'];
-//         $imageName = $_FILES['image']['name'];
-        
-//         // Check if the uploaded file is an image
-//         $imageType = exif_imagetype($tmpName);
-
-//         if ($imageType !== false) {
-//             $uploadDir = 'uploads/';
-//             $uploadPath = $uploadDir . $imageName;
-
-//             // Move the image to the uploads folder
-//             move_uploaded_file($tmpName, $uploadPath);
-
-      
-//             return $uploadPath;
-//         } else {
-//             echo "Invalid image format. Please upload a valid image.";
-//             // You may want to redirect or display an error message here
-//             exit();
-//         }
-//     }
-//     return null;
-// }
-
 
     
 ?>
