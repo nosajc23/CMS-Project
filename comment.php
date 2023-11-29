38f231cb5027470b001f418abe3524b8a9b6329f
Jason Castillo <122952780+nosajc23@users.noreply.github.com>
2023-11-28 23:13:23 -0600

    updated the comment.php

--- comment.php
+++ comment.php
@@ -30,11 +30,45 @@ function getComments($page) {
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
+// Function to generate a random CAPTCHA code
+function generateCaptchaCode($length = 5) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $code = '';
+    for ($i = 0; $i < $length; $i++) {
+        $code .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $code;
+}
+// Generate and store a new CAPTCHA code in the session if not already set
+if (!isset($_SESSION['captcha'])) {
+    $_SESSION['captcha'] = strtoupper(generateCaptchaCode());
+}
+
+echo "<img src='captcha_image.php' alt='CAPTCHA'>";
+
 // Handle form submission
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $comment = $_POST['comment'];
     $page = $_POST['page'];
+    $userCaptcha = strtoupper($_POST['captcha']); // Convert to uppercase for case-insensitive comparison
+
+    // Validate CAPTCHA
+    if (isset($_SESSION['captcha']) && $userCaptcha === $_SESSION['captcha']) {
+        // CAPTCHA is correct, process the comment
+        $comment = $_POST['comment'];
+        // Add your comment processing logic here
+
+        // Clear the CAPTCHA session for security
+        unset($_SESSION['captcha']);
+    } else {
+        // CAPTCHA validation failed, set an error message
+        $captchaError = "Invalid CAPTCHA. Please try again.";
+    }
+    // Generate and store a new CAPTCHA code in the session if validation failed
+    if (isset($captchaError)) {
+    $_SESSION['captcha'] = strtoupper(generateCaptchaCode());
+}
 
     // Validate input
     if (!empty($comment) && !empty($page)) {
@@ -46,9 +80,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-header('Location: UserPage.php');
-
+header("Location: UserPage.php");
 // Display comments for the current page
 $current_page = basename($_SERVER['PHP_SELF']);
 $comments = getComments($current_page);
+
+
 ?>
+
+
+
